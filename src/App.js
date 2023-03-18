8ca0ded25f4e34edbb79f61366f0e90226fa4846
Jitender <jitendersharmateharki@gmail.com>
2023-03-18 11:08:31 +0530

    10th Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+import About from './components/About';
 
 
 let name = "World!"
 function App() {
   return (
     <>
-      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-      <Navbar/>
-      <div className="container">
+      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      {/* <Navbar/> */}
+      {/* <div className="container">
         <TextForm heading="Enter the text below to analyze" />
-      </div>
+      </div> */}
+      <About />
     </>
   );
 }
